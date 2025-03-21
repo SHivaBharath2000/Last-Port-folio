7d569b23c0a48e6f1b4b21e80da33e0fe03c534f
siva <bharathsiva411@gmail.com>
2025-03-21 23:04:07 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Header } from "./components/header/Header";
 import { Topcontainer } from "./components/Topcontainer";
-import SkillContainer from "./components/skillcontainer/Skillconainer";
 import ProjectContainer from "./components/ProjectContainer/ProjectContainer";
 import Experiencecontainer from "./components/Experience Container/Experiencecontainer";
 import Contact from "./components/Contact/Conatct";
-// import Skillconainer from  "./components/skillcontainer/Skillconainer";
+import SkillContainer from "./components/skillcontainer/Skillcontainer";
 function App() {
 
 
